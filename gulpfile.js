c6d393c8cb47f23bff11715f658f866c77750909
Greg Walden <greg.walden@makerbot.com>
2016-01-28 22:10:11 -0500

    update to secure

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,60 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var less        = require('gulp-less');
-var minifyCSS   = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber     = require('gulp-plumber');
-var notify      = require('gulp-notify');
-var jade        = require('gulp-jade');
-var uglify      = require('gulp-uglify');
-var sourcemaps  = require('gulp-sourcemaps');
+'use strict';
+
+const gulp      = require('gulp')
+  , browserSync = require('browser-sync').create()
+  , less        = require('gulp-less')
+  , minifyCSS   = require('gulp-cssnano')
+  , autoprefixer = require('gulp-autoprefixer')
+  , plumber     = require('gulp-plumber')
+  , notify      = require('gulp-notify')
+  , jade        = require('gulp-jade')
+  , uglify      = require('gulp-uglify')
+  , sourcemaps  = require('gulp-sourcemaps')
+  ;
 
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: './'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
 });
 
 gulp.task('less', function () {
-    return gulp.src('css/src/*.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(autoprefixer())
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('css/dist'))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+  return gulp.src('css/src/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css/dist'))
+    .pipe(browserSync.stream())
+    .pipe(notify('Finished file: <%= file.relative %>'));
 });
 
 gulp.task('js', function () {
-    return gulp.src('js/src/*.js')
-        .pipe(plumber())
-        // .pipe(jshint())
-        // .pipe(jshint.reporter('default'))
-        .pipe(sourcemaps.init())
-        // .pipe(babel())
-        .pipe(uglify())
-        // .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('js/dist'))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+  return gulp.src('js/src/*.js')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('js/dist'))
+    .pipe(browserSync.stream())
+    .pipe(notify('Finished file: <%= file.relative %>'));
 });
 
 gulp.task('jade', function () {
-    return gulp.src('jade/index.jade')
-        .pipe(plumber())
-        .pipe(jade())
-        .pipe(gulp.dest(''))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
-});
-
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+  return gulp.src('jade/index.jade')
+    .pipe(plumber())
+    .pipe(jade())
+    .pipe(gulp.dest(''))
+    .pipe(browserSync.stream())
+    .pipe(notify('Finished file: <%= file.relative %>'));
 });
 
 // Default task to be run with `gulp`
 gulp.task('default', ['jade', 'less', 'browser-sync'], function () {
-    gulp.watch('css/src/*.less', ['less']);
-    gulp.watch('jade/index.jade', ['jade']);
-    gulp.watch('js/src/*.js', ['js']);
+  gulp.watch('css/src/*.less', ['less']);
+  gulp.watch('jade/index.jade', ['jade']);
+  gulp.watch('js/src/*.js', ['js']);
 });
